33b75b455372009dae5e87b35fa25489d0e91156
Кожевникова Светлана <kolgnik5@gmail.com>
2026-01-14 21:40:24 +0300

    Delete orders-tg-bot-master directory

--- orders-tg-bot-master/config.py
+++
@@ -1,7 +0,0 @@
-# Здесь изменить переменные под свои нужды
-ADMIN_CHAT_ID=[950904592]
-TP_CHAT_ID=[950904592111]
-MANAGER_CHAT_ID=[950904592111]
-ORDERS_CHAT_ID='https://t.me/+t5vlJnJiTYBmNjcy'
-BOT_TOKEN='7732899419:AAG-c_LIpYQAN9TGHNGvQialudShV4WVqYk'
-FILE_DB='C:\\Users\\Фотончик\\OneDrive\\Desktop\\домашка\\BKR\\08code\\orders-tg-bot-master\\orders.db'
